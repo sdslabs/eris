4a4d3c12a8d14a9aeaea850911df4c7cd2f59dda
Gurmannat Sohal <iamgurmannatsohal@gmail.com>
2023-01-30 00:41:28 +0530

    get, post requests on login page

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,70 @@
-import Button1 from "../components/button1";
 import Button2 from "../components/button2";
-import Input from "../components/input_box";
-import Password from "../components/password";
 import Labs from "../public/images/labs logo.png";
 import Image from "next/image";
 import Carousel from "../components/carousel";
-// import { useState,useEffect } from 'react';
-// import axios from 'axios';
-// import express from 'express';
-// import cors from 'cors';
-// var app = express()
- 
-// app.use(cors())
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+const eye = <FontAwesomeIcon icon={faEye} />;
+const crossedEye = <FontAwesomeIcon icon={faEyeSlash} />;
+
+var FlowID, CsrfToken
 
 const LoginPage = () => {
+  const [inputActive1, setInputActive1] = useState(false);
+  const [inputActive2, setInputActive2] = useState(false);
+  const [passwordShown, setPasswordShown] = useState(false);
+  const [eyeCrossed, setEyeCrossed] = useState(false);
+  const togglePasswordVisiblity = () => {
+    setPasswordShown(passwordShown ? false : true);
+  };
+  const toggleEye = () => {
+    setEyeCrossed(eyeCrossed ? false : true);
+  };
 
-// async function postDataToAPI(csrfToken, flowID, email, password) {
-//     try {
-//         const response = await axios.post('https://0.0.0.0:9999/login', csrfToken, flowID, email, password);
-//         console.log(response.csrfToken, response.flowID, response.email, response.password);
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
 
-// useEffect(() =>{
-//  const createFLow= async ()=>{
-//   const response=await fetch('http://0.0.0.0:9999/login')
-//   console.log(response.headers)
-//  }
-//  createFLow()
-// }, []);
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+
+  let sendRequest = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("http://0.0.0.0:9999/login", {
+        method: "POST",
+        body: JSON.stringify({
+          flowID: FlowID,
+          csrf_token: CsrfToken,
+          password: pass,
+          identifier: email,
+        }),
+
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setEmail("");
+        setPass("");
+        console.log();
+      } else {
+        setMessage("Some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+useEffect(() =>{
+ const createFLow= async ()=>{
+  const response=await fetch('http://0.0.0.0:9999/login')
+  const result = await response.json()
+  FlowID=(result.flowID)
+  CsrfToken=(result.csrf_token)
+  console.log(response.headers);
+ }
+ createFLow()
+}, []);
 
 return (
-  
+
 <div className="loginpage">
   <div className="split_left">
     <div className="top">
@@ -49,18 +80,47 @@ return (
         <h1>Log<span className="green">in</span></h1>
       </div>
       <div className="form">
+      <form onSubmit={sendRequest}>
         <div>
         <p>Email address</p>
-        <Input 
-          text="Enter your email address"/>
-          <p>Password</p>
-        <Password 
-          text="Enter your password"/>
+        <div className={"inputBox" + " " + inputActive1}>
+        <input
+          type="text"
+          onFocus={() => setInputActive1(!inputActive1)}
+          onBlur={() => setInputActive1(!inputActive1)}
+          className="input"
+          placeholder="Enter your email address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        >
+    </input>
+    </div>
+        <p>Password</p>
+
+        <div className={"inputBox"+ " " + inputActive2}>
+        {" "}
+        <input
+          type={passwordShown ? "text" : "password"}
+          className="input"
+          placeholder="Enter your password"
+          value={pass}
+          onFocus={() => setInputActive2(!inputActive2)}
+          onBlur={() => setInputActive2(!inputActive2)}
+          onChange={(e) => setPass(e.target.value)}
+        >
+        </input>
+        <i className="passEye" onClick={() =>
+          { toggleEye(); togglePasswordVisiblity();}}>
+            {eyeCrossed ? eye : crossedEye}</i>{" "}
+    </div>
+    {/* <p className="text-danger">{passwordError}</p> */}
+
+
         </div>
         <div className="tickBox">
-          <input 
-            type="checkbox" 
-            className="checkbox" 
+          <input
+            type="checkbox"
+            className="checkbox"
           />
           <label className="remember">Remember me</label>
           <a className=" underline green" href="/recover" style={{float: "right"}}>
@@ -68,13 +128,12 @@ return (
           </a>
         </div>
         <div>
-          <Button1
-            text={"Login"}
-            //onclick={postDataToAPI}
-            //onclick={Example}
-          />
+        <button type="submit" className="button1">
+          Login
+        </button>
         </div>
-          <p> 
+        </form>
+          <p>
             Don't have an account? <a className="green underline" href="/signup">
             Sign up </a>
           </p>
