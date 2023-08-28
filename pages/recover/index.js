446f660e978995bb87655521e81326017bdcffab
NehaGujar1 <gujarnehap@gmail.com>
2023-08-28 18:20:18 +0530

    integrate recover page changes

--- pages/recover/index.js
+++ pages/recover/index.js
@@ -1,10 +1,47 @@
 import ButtonSubmit from "../../components/button_submit";
+import axios from "axios";
 import Input from "../../components/input_box";
 import Labs from "../../public/images/labs logo.png";
 import Image from "next/image";
 import Carousel from "../../components/carousel";
+import { React, useState } from "react";
+import { Button } from "@mui/material";
 
 const RecoveryPage = () => {
+    const [inputActive1, setInputActive1] = useState(false);
+    const [email, resetPassword] = useState("");
+
+    let sendRequest = async () => {
+        try {
+          const getResponse = await axios.get(process.env.NEXT_PASSWORD_RESET, {
+            withCredentials: true,
+          });
+          const res = await axios.post(
+            process.env.NEXT_PASSWORD_RESET,
+            {
+              flowID: getResponse.data.flowID,
+              csrf_token: getResponse.data.csrf_token,
+              identifier: email,
+              method:"",
+            },
+            {
+              withCredentials: true,
+            }
+          );
+          if (res.status === 200) {
+            resetPassword("");
+            console.log("reset link sent");
+            return true
+          } else {
+            setMessage("some error occured");
+            return false
+          }
+        } catch (err) {
+          console.log(err);
+          return false
+        }
+      };
+    
   return (
 <div>
     <div className="split_left">
@@ -23,14 +60,35 @@ const RecoveryPage = () => {
             <div className="form">
                 <div>
                     <p>Email address</p>
-                    <Input
-                        text={"Enter your registered email address"}
-                    />
+                    <div className={"inputBox" + " " + inputActive1}>
+                        <input
+                        type="text"
+                        onFocus={() => setInputActive1(!inputActive1)}
+                        onBlur={() => setInputActive1(!inputActive1)}
+                        className="input"
+                        placeholder="Enter your registered email address"
+                        value={email}
+                        onChange={(e) => resetPassword(e.target.value)}
+                        ></input>
+                    </div>
                 </div>
                 <div>
-                    <ButtonSubmit
+                    <button 
+                        type="submit"
                         text={"Send reset link"}
-                    />
+                        className="button_submit"
+                        onClick={async ()=>{
+                            console.log("request created")
+                            if(await sendRequest()){
+                              console.log("redirect");
+                               redirect();
+                            }else{
+                              console.log("error")
+                            }
+                          }}
+                    >
+                    Send reset link
+                    </button>
                 </div>
             </div>
         </div>
