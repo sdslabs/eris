4930fe4238020ee35952711f561448e23520fdef
Ashutosh <cyberboy9971@gmail.com>
2023-08-30 00:37:41 +0530

    ChangeBox functionality in editAppBox

--- components/editAppBox.js
+++ components/editAppBox.js
@@ -6,33 +6,60 @@ import CopyIcon from "../public/images/copyicon.svg"
 
 const EditAppBox = ({ img, name, des, href, domains, organisations, handleAppBox}) => {
 
-    const [text, setText] = useState("");
+  const [text, setText] = useState("");
+  const [image, setImage] = useState(null);
 
-    const [selectedItems, setSelectedItems] = useState([]);
-    const [dropdownValue, setDropdownValue] = useState("");
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [dropdownValue, setDropdownValue] = useState("");
 
-    const handleSelect = (event) => {
-        const selectedItem = event.target.value;
-        setSelectedItems([...selectedItems, selectedItem]);
-        setDropdownValue("");
-    };
+  const handleSelect = (event) => {
+    const selectedItem = event.target.value;
+    setSelectedItems([...selectedItems, selectedItem]);
+    setDropdownValue("");
+  };
 
-    const handleDelete = (itemToDelete) => {
-        const newSelectedItems = selectedItems.filter(
-        (item) => item !== itemToDelete
-        );
-        setSelectedItems(newSelectedItems);
-    };
+  const handleDelete = (itemToDelete) => {
+    const newSelectedItems = selectedItems.filter((item) => item !== itemToDelete);
+    setSelectedItems(newSelectedItems);
+  };
+
+  const handleChangeImage = (event) => {
+    const newImageFile = event.target.files[0];
+
+    if (newImageFile) {
+      setImage(URL.createObjectURL(newImageFile));
+    }
+  };
+
+  const handleRemoveImage = () => {
+    setImage(null);
+  };
 
   return (
     <div className="app_box">
       <div className="app_topbox">
         <div className="app_image">
+          {image ? (
+            <img className="app_image" src={image} alt="test" />
+          ) : (
             <Image className="app_image" src={img} alt="test" />
+          )}
         </div>
         <div className="app_data">
-            <p style={{cursor: "pointer"}} className="underline"><b>Change Image</b></p>
-            <p style={{cursor: "pointer"}} className="underline">Remove Image</p>
+          <label htmlFor="changeImage" className="underline">
+            <b>Change Image</b>
+          </label>
+          <input
+            type="file"
+            id="changeImage"
+            accept="image/*"
+            onChange={handleChangeImage}
+            style={{ display: "none" }}
+          />
+          <br />
+          <label className="underline" onClick={handleRemoveImage} style={{ cursor: "pointer" }}>
+            Remove Image
+          </label>
         </div>
       </div>
       <div>
