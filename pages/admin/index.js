743a96d41ea68277bd183c89834c5c87610c066c
NehaGujar1 <gujarnehap@gmail.com>
2023-09-05 02:49:36 +0530

    Link invites page with admin page

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -7,6 +7,7 @@ import InvitesTable from "../../components/invites_table";
 import UserAdd from "../../public/images/user_add.svg";
 import Filter from "../../public/images/filter.svg";
 import data from "../../data/users_data.json";
+import InvitesData from "../../data/invites_data.json";
 import AddUser from "../../components/add_user.js";
 import Underline from "../../public/images/active_tab.svg"
 import Image from "next/image";
@@ -20,29 +21,50 @@ const AdminPage = () => {
   {invitesActive ? (ifInviteActive="setLine", ifUserActive=null ) : (ifInviteActive=null ,ifUserActive="setLine")}
   const [adminChecked, setAdminChecked] = useState(false);
   const [userChecked, setUserChecked] = useState(false);
-  const [userData, setUserData] = useState(data);
-  const [totalData, setTotalData] = useState(data);
+  const [userData, setUserData] = useState([]);
+  const [invitesData, setInviteData] = useState(InvitesData);
+  const [userTotalData, setUserTotalData] = useState(data);
   const [UTable, setUTable] = useState(<UserTable userData={[]}/>);
-  const [ITable, setITable] = useState(<InvitesTable userData={[]}/>);
+  const [ITable, setITable] = useState(<InvitesTable invitesData={InvitesData}/>);
+  console.log("invitesActive");
+  console.log(invitesActive);
   function updateOnSearch(inputText){
     var recentData=[];
     if (inputText.length > 0) {
-    data.filter((user) => {
+      if(!invitesActive) {
+        data.filter((user) => {
       if(user.name.match(inputText)) recentData.push(user);
       return user.name.match(inputText);
     });
     }
-    else recentData = data;
-    var roleData=[];
-    recentData.filter((user) => {
+    else{
+      InvitesData.filter((user) => {
+        if(user.name.match(inputText)) recentData.push(user);
+        return user.name.match(inputText);
+      });
+    }
+  }
+    else if(!invitesActive) recentData = data;
+    else recentData = InvitesData;
+    if(!invitesActive) {
+      var roleData=[];
+      recentData.filter((user) => {
       if(user.role.match("admin")&&adminChecked) roleData.push(user);
       else if(user.role.match("user")&&userChecked) roleData.push(user);
       return user.name.match("admin");
     },);
+    console.log("roleData");
     console.log(roleData);
     setUserData(roleData);
-    setTotalData(recentData);
+    setUserTotalData(recentData);
     setUTable(<UserTable userData={roleData}/>);
+  }
+  else{
+    setInviteData(recentData);
+    console.log("roleData invite");
+    console.log(roleData);
+    setITable(<InvitesTable invitesData={recentData}/>);
+  }
     console.log(roleData);
     console.log("inside updateOnSearch");
     console.log(recentData);
@@ -51,8 +73,8 @@ const AdminPage = () => {
 function AdminRole ( role ) {
   var roleData=[];
   console.log("inside AdminRole");
-  console.log(totalData);
-  totalData.filter((user) => {
+  console.log(userTotalData);
+  userTotalData.filter((user) => {
     var adminm = String("admin");
     var userm = String("user");
     var adminCheckedVal=adminChecked;
@@ -95,11 +117,18 @@ return (
     <hr className="admin_hr"/>
     <div className="search_panel">
     <Searchbar updateOnSearch={updateOnSearch}/>
-    <div className="roles">
+    {(() => {
+      console.log("hii"); 
+      if(!invitesActive){
+      return(
+    <div className="roles" >
         <b>Role </b>
         <input type="checkbox" style={{ marginLeft: "1rem" }} id="admin_role" onChange={()=>{setAdminChecked(!adminChecked); AdminRole("admin")}}/> Admin
         <input type="checkbox" style={{ marginLeft: "2rem" }} id="user_role" onChange={()=>{setUserChecked(!userChecked); AdminRole("user")}}/> User
       </div>
+      )
+}
+})()}
     <Buttons
       text1="Filter"
       text2="Add User"
@@ -107,8 +136,8 @@ return (
       img2={UserAdd}/>
     </div>
     <div className="data_div">
-      {UTable}
-    {invitesActive ? <InvitesTable /> : <UserTable />}
+      {/* {UTable} */}
+    {invitesActive ? ITable : UTable}
     </div>
     </div> </div>
   );
