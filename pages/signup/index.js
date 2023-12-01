ded8ac4370f26e0291232f556892e6f42c46789d
Aryan Singh <krish7664@gmail.com>
2023-12-01 22:33:24 +0530

    Fixes minor bug

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,26 +1,25 @@
-import { React, useState } from 'react';
+import { React, useState } from "react";
 import Icons from "../../components/icons";
-import IconsPass from '../../components/icons_pass';
-import IconsVerify from '../../components/icons_verify';
+import IconsPass from "../../components/icons_pass";
+import IconsVerify from "../../components/icons_verify";
 import Labs from "../../public/images/labs logo.png";
 import Image from "next/image";
 import Carousel from "../../components/carousel";
-import SetPassword from '../../components/setpass';
-import Signup from '../../components/register';
-import Verify from '../../components/verifyEmail';
+import SetPassword from "../../components/setpass";
+import Signup from "../../components/register";
+import Verify from "../../components/verifyEmail";
 
 const SignupPage = () => {
   const [showSignup, setShowSignup] = useState(true);
   const [showPasswordScreen, setShowPasswordScreen] = useState(true);
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
   const handleButtonClick = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     setShowSignup(!showSignup);
   };
   const handlePasswordButton = (event) => {
-    event.preventDefault()
     setShowPasswordScreen(!showPasswordScreen);
   };
 
@@ -35,39 +34,55 @@ const SignupPage = () => {
         </div>
       </div>
       <div className="split_right ">
-        <div className='signup'>
-        {(() => {
-        if (showSignup) {
-          return (
-            <div>
-              <div>
-                <Icons />
-              </div>
-              <Signup handleClick={handleButtonClick} name={name} email={email} number={number} setEmail={setEmail} setName={setName} setNumber={setNumber}/>
-            </div>
-          )
-        } else if (!showSignup && showPasswordScreen) {
-          return (
-            <div>
-              <div>
-                <IconsPass/>
-              </div>
-              <SetPassword handleClick={handlePasswordButton} className='slide-in' name={name} email={email} number={number} setEmail={setEmail} setName={setName} setNumber={setNumber}/>
-            </div>
-          )
-        } else {
-          return (
-            <div>
-              <div>
-                <IconsVerify/>
-              </div>
-              <Verify className='slide-in' email={email}/>
-            </div>
-          )
-        }
-      })()}
-          </div>
-
+        <div className="signup">
+          {(() => {
+            if (showSignup) {
+              return (
+                <div>
+                  <div>
+                    <Icons />
+                  </div>
+                  <Signup
+                    handleClick={handleButtonClick}
+                    name={name}
+                    email={email}
+                    number={number}
+                    setEmail={setEmail}
+                    setName={setName}
+                    setNumber={setNumber}
+                  />
+                </div>
+              );
+            } else if (!showSignup && showPasswordScreen) {
+              return (
+                <div>
+                  <div>
+                    <IconsPass />
+                  </div>
+                  <SetPassword
+                    handleClick={handlePasswordButton}
+                    className="slide-in"
+                    name={name}
+                    email={email}
+                    number={number}
+                    setEmail={setEmail}
+                    setName={setName}
+                    setNumber={setNumber}
+                  />
+                </div>
+              );
+            } else {
+              return (
+                <div>
+                  <div>
+                    <IconsVerify />
+                  </div>
+                  <Verify className="slide-in" email={email} />
+                </div>
+              );
+            }
+          })()}
+        </div>
       </div>
     </div>
   );
