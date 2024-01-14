2c8d3014e6d763791adeff0183c11ba48a73c7a8
Aryan Singh <krish7664@gmail.com>
2024-01-14 09:23:50 +0530

    Fixes left panel of admin page

--- components/leftPanel.js
+++ components/leftPanel.js
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { handleGetLogoutFlow, handlePostLogoutFlow } from "../api/logoutFlow";
+import { handleGetSessionDetailsFlow } from "../api/profileFlow";
 import Applications from "../public/images/applications.svg";
 import Applications_self from "../public/images/applications_self.svg";
 import Labs from "../public/images/labs logo.png";
@@ -10,15 +13,33 @@ import Test from "../public/images/test.jpg";
 import Users from "../public/images/users.svg";
 import Users_self from "../public/images/users_self.svg";
 
-const LeftPanel = ({ page, activity1, activity2, activity3, state1, state2, state3 }) => {
-  var User, Application;
-  if (page == "user") {
-    User = Users_self;
-    Application = Applications;
-  } else if (page == "applications") {
-    User = Users;
-    Application = Applications_self;
+function LeftPanel({ page, activity1, activity2, activity3, state1, state2, state3 }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    async function getProfileDetails() {
+      const { name, email } = await handleGetSessionDetailsFlow();
+      setName(name);
+      setEmail(email);
+    }
+    getProfileDetails();
+  }, []);
+
+  const User = page === "user" ? Users_self : Users;
+  const Application = page === "user" ? Applications : Applications_self;
+
+  async function handleLogout() {
+    try {
+      const logoutToken = await handleGetLogoutFlow();
+      await handlePostLogoutFlow(logoutToken);
+      router.push("/");
+    } catch (err) {
+      console.error(err);
+    }
   }
+
   return (
     <div className="left_panel">
       <div className="centre" style={{ width: "8.2rem" }}>
@@ -41,13 +62,15 @@ const LeftPanel = ({ page, activity1, activity2, activity3, state1, state2, stat
           <Image className="logout_image" src={Test} alt="test" />
         </div>
         <div className="logout_text">
-          <p style={{ color: "white", fontSize: "1.3rem" }}>Ada Merlin</p>
-          <p style={{ color: "white" }}>user@gmail.com</p>
+          <p style={{ color: "white", fontSize: "1.3rem" }}>{name}</p>
+          <p style={{ color: "white" }}>{email}</p>
         </div>
-        <Image className="logout_logo" src={Logout} alt="logout" />
+        <button onClick={handleLogout}>
+          <Image className="logout_logo" src={Logout} alt="logout" />
+        </button>
       </div>
     </div>
   );
-};
+}
 
 export default LeftPanel;
