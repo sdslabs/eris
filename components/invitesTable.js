f835da96ad24e716f38c7994d5d0886bbd24f7e5
Aryan Singh <krish7664@gmail.com>
2024-01-14 09:26:00 +0530

    Adds and refactors pagination of User and Invite table

--- components/invitesTable.js
+++ components/invitesTable.js
@@ -1,26 +1,33 @@
 import Image from "next/image";
 import { React, useState } from "react";
-import bannedUser from "../public/images/banned.svg";
+import AcceptedUser from "../public/images/accepted.svg";
 import defaultFace from "../public/images/default_face.svg";
+import PendingUser from "../public/images/pending.svg";
 import Pagination from "./pagination";
 import UserPopup from "./user_mgmt";
 
 function Table({ currentPost }) {
   return (
-    <table className="user_table">
+    <table className="invites_table">
       <tbody>
         <tr>
-          <th style={{ width: "25%" }}>
+          <th style={{ width: "20%" }}>
             <b>NAME</b>
           </th>
           <th style={{ width: "10%" }}></th>
-          <th style={{ width: "30%" }}>
+          <th style={{ width: "25%" }}>
             <b>EMAIL</b>
           </th>
           <th style={{ width: "10%" }}>
             <b>ROLE</b>
           </th>
-          <th style={{ width: "20%" }}>
+          <th style={{ width: "10%" }}>
+            <b>DATE INVITED</b>
+          </th>
+          <th style={{ width: "10%" }}>
+            <b>INVITED BY</b>
+          </th>
+          <th style={{ width: "10%" }}>
             <b>GITHUB</b>
           </th>
           <th style={{ width: "5%" }}></th>
@@ -28,15 +35,22 @@ function Table({ currentPost }) {
         {currentPost.map((Val) => {
           return (
             <>
-              <tr key={Val.id} className="users_data">
+              <tr key={Val.id} className="invites_data">
                 <td style={{ verticalAlign: "middle" }}>
                   {" "}
-                  <Image src={defaultFace} alt="profile" /> {Val.traits.name}
+                  <Image src={defaultFace} alt="" /> {Val.traits.name}
                 </td>
-                <td> {Val.state === "inactive" ? <Image src={bannedUser} alt="banned user" /> : null} </td>
+                <td>
+                  {Val.traits.invite_status === "pending" ? (
+                    <Image src={PendingUser} alt="pending user" />
+                  ) : (
+                    <Image src={AcceptedUser} alt="accepted user" />
+                  )}{" "}
+                </td>
                 <td> {Val.traits.email} </td>
                 <td> {Val.traits.role} </td>
-                <td> {Val.traits.github} </td>
+                <td> {Val.traits.date} </td>
+                <td> {Val.traits.invitedby} </td>
                 <td>
                   <UserPopup identityId={Val.id} />
                 </td>
@@ -49,8 +63,8 @@ function Table({ currentPost }) {
   );
 }
 
-function UserTable({ userData }) {
-  const post = userData;
+function InvitesTable({ invitesData }) {
+  const post = invitesData;
   const [number, setNumber] = useState(1); // No of pages
   const [postPerPage, setPostPerPage] = useState({ value: 2, label: "2/Page" });
 
@@ -73,4 +87,4 @@ function UserTable({ userData }) {
   );
 }
 
-export default UserTable;
+export default InvitesTable;
